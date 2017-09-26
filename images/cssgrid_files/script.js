edf3d7f55ef319d11eb1421078e7386d22ccfeb4
Brigitte Jellinek via Travis CI <brigitte.jellinek@fh-salzburg.ac.at>
2017-09-26 13:46:51 +0000

    cssgrid example finished (originally 0d7f470)

--- images/cssgrid_files/script.js
+++ images/cssgrid_files/script.js
@@ -1,18 +1,27 @@
+function insert_stylesheet_link(path,display) {
+  let filename = path.replace('cssgrid_files/','');
+  let h = "Stylesheet <a href='" + path + "'>" + filename + "</a>";
+  console.log(`inserting html ${h} into ${display}`);
+  $(display).html(h);
+
+}
+
 function cssLoaded() {
   display_css_properties_of('.page_container', '#show_css', ["display","grid-template-columns", "grid-template-rows", "grid-template-areas"]);
   let path = $('#grid_sheet').attr('href');
-  let filename = path.replace('cssgrid_files/','');
-  $('#show_name').html("Stylesheet " + filename );
+  insert_stylesheet_link(path, '#show_name');
 }
 
 
 function display_css_properties_of(to_inspect, display, list_of_props) {
   var s1 = "";
   list_of_props.forEach(function(key) {
-    s1 += "<b>" + key + "</b>: " +  ($(to_inspect).css(key) || 0) + ";<br>\n";
+    let v = ($(to_inspect).css(key) || 0);
+    if ( v.match(/"/) ) { v = v.replace(/" "/g, '"<br>"'); }
+    s1 += "<b>" + key + ":</b> " +  v + ";<br>\n";
   })
   $(display).html(s1);
-}      
+}
 
 
 function set_css(name, callback, display) {
@@ -21,9 +30,9 @@ function set_css(name, callback, display) {
   $('#grid_sheet').attr({'href': path});
 
   let img = document.createElement('img');
-  img.onerror = function(){  
+  img.onerror = function(){
     console.log(`stylesheet ${filename} was loaded !`);
-    $(display).html("Stylesheet " + filename);
+    insert_stylesheet_link(path, display);
     callback();
   };
   img.src = filename;
